e8c56c995d8805ef6536b324a0f35fa3b68a2dec
EkainDW <esusperregiib18dw@ikzubirimanteo.com>
2018-11-16 08:29:39 +0100

    20181116

---
+++ src/konexioproba/ariketa1.java
@@ -0,0 +1,14 @@
+package konexioproba;
+
+public class ariketa1 {
+
+	public static void main(String[] args) {
+		String nombre = "Ekain";
+		
+		for (int i=1 ; i<=nombre.length() ; i++) {
+			System.out.println(nombre.substring(0,i));
+		}
+
+	}
+
+}
